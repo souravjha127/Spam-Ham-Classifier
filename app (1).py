ec908e5aab06a4bab72e20ee79870e148d9977fb
Sourav Kumar Jha <121681492+souravjha127@users.noreply.github.com>
2025-08-10 12:18:14 +0530

    app.py

--- app (1).py
+++ app (1).py
@@ -21,5 +21,4 @@ add_bg_from_url()
 ip = st.text_input('Enter the message') #creates a text box in web app
 op = model.predict([ip])
 if st.button('Predict'):
-  st.title(op[0]) # st.button create a button with name predict
-  #st.title(op[0]) # the output will be displayed as a title
+  st.title(op[0]) 
